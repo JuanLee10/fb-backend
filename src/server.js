2c92cc84e3641c10de216fd2153373579f9518d1
Juan Lee <69443239+JUANie10@users.noreply.github.com>
2022-05-26 06:13:03 -0700

    procfile

--- src/server.js
+++ src/server.js
@@ -3,5 +3,5 @@ const app = require('./app.js');
 
 app.listen(process.env.PORT || 3010, () => {
   console.log(`Server Running on port 3010`);
-  console.log('API Testing UI: http://localhost:3010/v0/api-docs/');
+  console.log('API Testing UI: https://fakefacebookmarketplace-be.herokuapp.com/v0/api-docs/');
 });
